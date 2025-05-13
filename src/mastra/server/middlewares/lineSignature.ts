aa0089692bade612027676d198a68c091b44dc11
TatsuyaYamamoto <t28.is.tatsuya@gmail.com>
2025-05-14 01:40:55 +0900

    chore: add webhook

---
+++ src/mastra/server/middlewares/lineSignature.ts
@@ -0,0 +1,22 @@
+import { validateSignature } from "@line/bot-sdk";
+
+import { LINE_CHANNEL_SECRET } from "../../env";
+import type { MiddlewareHandler } from "../../types";
+
+export const lineSignatureMiddleware: MiddlewareHandler = async (
+  c,
+  next,
+): Promise<Response | void> => {
+  const xLineSignature = c.req.header("x-line-signature");
+  const body = await c.req.text();
+
+  if (
+    LINE_CHANNEL_SECRET &&
+    xLineSignature &&
+    !validateSignature(body, LINE_CHANNEL_SECRET, xLineSignature)
+  ) {
+    return c.json({ message: "Unauthorized" }, { status: 401 });
+  }
+
+  await next();
+};
