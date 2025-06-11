b9f48d586122c2f11d25319f8861b10bd7c95636
TatsuyaYamamoto <t28.is.tatsuya@gmail.com>
2025-06-12 01:03:27 +0900

    feat: set `DEV` as env var

--- src/mastra/env.ts
+++ src/mastra/env.ts
@@ -1,15 +1,18 @@
 import { z } from "zod";
 
 const envVariables = z.object({
+  DEV: z
+    .string()
+    .optional()
+    .transform((value) => value === "true"),
   TOKEN: z.string(),
   LINE_CHANNEL_SECRET: z.string(),
   LINE_MESSAGING_API_CHANNEL_ACCESS_TOKEN: z.string(),
 });
 
 export const {
+  DEV,
   TOKEN,
   LINE_CHANNEL_SECRET,
   LINE_MESSAGING_API_CHANNEL_ACCESS_TOKEN,
 } = envVariables.parse(process.env);
-
-export const isDev = process.env["NODE_ENV"] === "development";
