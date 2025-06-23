c783824c68c9ee7d2a4267234a257281f80552fb
Tatsuya Yamamoto / T28_tatsuya <t28.is.tatsuya@gmail.com>
2025-06-23 22:04:33 +0900

    feat: use d1 as mastra storage (#19)

--- scripts/postbuild.js
+++ scripts/postbuild.js
@@ -3,7 +3,7 @@ import path from "node:path";
 
 const mastraOutputDir = path.resolve(import.meta.dirname, "../.mastra/output");
 
-/**
+/*******************************************************************************
  * mastra のビルド成果物 (index.mjs) では dynamic import を行っており、`import()` には文字列リテラルではなく変数を渡している
  * @see https://github.com/mastra-ai/mastra/blob/%40mastra%2Fdeployer-cloudflare%400.1.20/packages/deployer/src/server/index.ts#L92
  *
@@ -40,12 +40,41 @@ const mastraIndexMjsContentReplaced = mastraIndexMjsContent
   );
 fs.writeFileSync(mastraIndexMjsPath, mastraIndexMjsContentReplaced);
 
+/*******************************************************************************
+ * Mastra の Storage クラスに渡す binding は mastra build 後、 top-level scope で定義される。
+ * Workers は request context 外での I/O を許可していないため、D1 の binding も request context 内で参照する必要がある。
+ * この GAP を埋めるために mastra が作成した index.mjs を dynamic import する main.mjs を作成して、binding を request context 内で参照されるようにする。
+ * @see https://developers.cloudflare.com/changelog/2025-03-17-importable-env/
+ */
+fs.writeFileSync(
+  path.resolve(mastraOutputDir, "main.mjs"),
+  `
+export default {
+  fetch: async (request, env, context) => {
+    const { default: entry } = await import("./index.mjs");
+    return entry.fetch(request, env, context);
+  }
+};
+`,
+);
+
+const mastraMjsPath = path.resolve(mastraOutputDir, "mastra.mjs");
+const mastraMjsContent = fs.readFileSync(mastraMjsPath, "utf-8");
+fs.writeFileSync(
+  mastraMjsPath,
+  `
+import { env } from "cloudflare:workers";
+${mastraMjsContent}
+`,
+);
+
 /**
  * wrangler.json の編集
  */
 const wranglerJsonPath = path.resolve(mastraOutputDir, "wrangler.json");
 const wranglerJson = JSON.parse(fs.readFileSync(wranglerJsonPath, "utf-8"));
 Object.assign(wranglerJson, {
+  main: "./main.mjs",
   assets: {
     directory: "./assets/",
   },
--- src/mastra/index.ts
+++ src/mastra/index.ts
@@ -1,10 +1,16 @@
+import { D1Store } from "@mastra/cloudflare-d1";
 import { Mastra } from "@mastra/core";
 import { ConsoleLogger } from "@mastra/core/logger";
 import { CloudflareDeployer } from "@mastra/deployer-cloudflare";
-import { LibSQLStore } from "@mastra/libsql";
 
 import { fairy } from "./agents/fairy";
 import { weatherAgent } from "./agents/weatherAgent";
+import {
+  CLOUDFLARE_ACCOUNT_ID,
+  CLOUDFLARE_API_TOKEN,
+  CLOUDFLARE_D1_DATABASE_ID,
+  DEV,
+} from "./env";
 import apiLineMessagesWebhook from "./server/api/agents.fairy.line.webhook";
 import { authMiddleware } from "./server/middlewares/auth";
 import { weatherWorkflow } from "./workflows";
@@ -12,9 +18,21 @@ import { weatherWorkflow } from "./workflows";
 export const mastra = new Mastra({
   workflows: { weatherWorkflow },
   agents: { fairy, weatherAgent },
-  storage: new LibSQLStore({
-    // stores telemetry, evals, ... into memory storage, if it needs to persist, change to file:../mastra.db
-    url: ":memory:",
+  storage: new D1Store({
+    // @ts-expect-error
+    ...(typeof env !== "undefined"
+      ? // for workers, binding is available
+        {
+          // @ts-expect-error --- it's ok because `import { env } from "cloudflare:workers";` is inserted into the bundle file at build time.
+          binding: env.MastraStorage,
+        }
+      : // for local development
+        {
+          accountId: CLOUDFLARE_ACCOUNT_ID,
+          apiToken: CLOUDFLARE_API_TOKEN,
+          databaseId: CLOUDFLARE_D1_DATABASE_ID,
+        }),
+    tablePrefix: DEV ? "dev_" : "",
   }),
   logger: new ConsoleLogger({
     name: "Mastra",
@@ -30,5 +48,12 @@ export const mastra = new Mastra({
     auth: {
       apiToken: "***",
     },
+    d1Databases: [
+      {
+        binding: "MastraStorage",
+        database_name: "sicaco-3rd",
+        database_id: CLOUDFLARE_D1_DATABASE_ID,
+      },
+    ],
   }),
 });
